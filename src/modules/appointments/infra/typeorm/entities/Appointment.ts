83845539cc3a518aaa7a8f82dce01a0be932bb80
Josimar16 <josimarjr479@gmail.com>
2020-10-01 13:05:29 -0300

    projeto estruturado com SOLID, DDD e testes automatizados

---
+++ src/modules/appointments/infra/typeorm/entities/Appointment.ts
@@ -0,0 +1,29 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
+import User from '@modules/users/infra/typeorm/entities/User'
+
+// KISS - Keep It Simple & Stupid
+// Matenha simples e estupido o seu código
+
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column()
+  provider_id: string
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User
+
+  @Column('timestamp with time zone')
+  date: Date
+
+  @CreateDateColumn()
+  create_at: Date
+
+  @UpdateDateColumn()
+  update_at: Date
+}
+
+export default Appointment
---
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,33 @@
+import ICreateUserDTO from "@modules/users/dtos/ICreateUserDTO";
+import IUsersRepository from "@modules/users/repositories/IUsersRepository";
+import { getRepository, Repository } from "typeorm";
+import User from "../entities/User";
+
+class UsersRepository implements IUsersRepository {
+  private ormRepository: Repository<User>
+  constructor() {
+    this.ormRepository = getRepository(User)
+  }
+  public async create({ name, email, password }: ICreateUserDTO): Promise<User> {
+    const user = this.ormRepository.create({ name, email, password })
+    await this.ormRepository.save(user)
+    return user
+  }
+
+  public async save(user: User): Promise<User> {
+    return this.ormRepository.save(user)
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    const findUser = await this.ormRepository.findOne(id)
+    return findUser
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const findUser = await this.ormRepository.findOne({ where: { email } })
+    return findUser
+  }
+
+}
+
+export default UsersRepository
