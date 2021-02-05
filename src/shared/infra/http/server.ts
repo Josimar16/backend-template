6ba5c05cc2967f45a04710eb81b77c0345fdab31
Josimar16 <josimarjr479@gmail.com>
2021-02-05 00:24:09 -0300

    adicionado validação em todas as rotas que não houvesse imagem, usando a biblioteca celebrate

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import { errors } from 'celebrate';
 import 'express-async-errors';
 import uploadConfig from '@config/upload';
 import '@shared/infra/typeorm';
@@ -9,11 +11,13 @@ import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 
 app.use(routes);
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError)
