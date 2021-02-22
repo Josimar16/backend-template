75b47627860a1774c3e8998fa35f5c650764ed3a
Josimar Martins <josimarjr479@gmail.com>
2021-02-22 14:01:39 -0300

    adicionando configuração para responsividade e variaveis para color

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -1,20 +1,45 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+  :root {
+    font-size: 60%;
+    --color-background: #312E38;
+    --color-primary: #28262E;
+    --color-error: #E53030;
+    --color-secundary: #FF9000;
+    --color-complement: #3E3B47;
+    --color-text-complement: #999591;
+    --color-button-text: #F4EDE8;
+    --color-calendar-week: #666360;
+    --color-toast-success: #2E656A;
+    --color-toast-success-light: #E6FFFA;
+    --color-toast-info: #3172B7;
+    --color-toast-info-light: #EBF8FF;
+    --color-toast-error: #C53030;
+    --color-toast-error-light: #FDDEDE;
+  }
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     outline: 0;
   }
+  html, body, #root {
+    height: 100vh;
+  }
   body {
-    background-color: #312E38;
-    color: #FFF;
+    background-color: var(--color-background);
     -webkit-font-smoothing: antialised;
   }
-  body, input, button {
+  #root {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  body, input, button, textarea {
     font-family: 'Roboto Slab', serif;
-    font-size: 16px;
+    color: var(--color-text);
+    font: 500 1.6rem;
   } 
   h1, h2, h3, h4, h5, h6, b, strong {
     font-weight: 500;
@@ -22,4 +47,9 @@ export default createGlobalStyle`
   button {
     cursor: pointer;
   }
+  @media (min-width: 720px) {
+     :root {
+        font-size: 62.5%;
+    }
+}
 `;
